7d0dc8de7584c14cc0dd09a9a86e255505b308cc
rivers729 <manda729@gmail.com>
2014-10-05 13:41:52 +0800

    Initial Setup

--- make.sh
+++ make.sh
@@ -1,31 +1,16 @@
-#!/bin/bash
-
-# Stupid shell script to compile kernel, nothing fancy
-
-# Exports all the needed things Arch, SubArch and Cross Compile
 export ARCH=arm
-echo 'exporting Arch'
-export SUBARCH=arm
-echo 'exporting SubArch'
-export CROSS_COMPILE=/home/prbassplayer/aosp/prebuilt/linux-x86/toolchain/linaro-4.8-13.06/bin/arm-linux-gnueabihf-
-#export CROSS_COMPILE=/home/prbassplayer/WIP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-echo 'exporting Cross Compile'
-
-# Make sure build is clean!
-echo 'Cleaning build'
-make clean
-
-# Generates a new .config and exists
-if [ "$1" = "config" ] ; then
-echo 'Making defconfig for Mako'
+unset CROSS_COMPILE
+#export CROSS_COMPILE=/opt/gcc-4.8-linaro/bin/arm-cortex_a15-linux-gnueabi-
+#export CROSS_COMPILE=/opt/gcc-4.7-linaro/bin/arm-cortex_a15-linux-gnueabi-
+export CROSS_COMPILE=/home/hafizan/opt/arm-eabi-4.8-sm/bin/arm-eabi-
+#echo "Cleaning old craps..."
+#make distclean
+export KBUILD_BUILD_USER=hafizan
+export KBUILD_BUILD_HOST="hanafi"
+export LOCALVERSION="-KleenKernel-Test"
+#echo "Copy backup config..."
+#cp najmi-mako-config .config
 make slim_mako_defconfig
-exit
-fi
-
-# Exports kernel local version? Not sure yet.
-#echo 'Exporting kernel version'
-#export LOCALVERSION='SlimTest_1.0'
-
-# Lets go!
-echo 'Lets start!'
-make -j$1
+make menuconfig
+#echo "Begin compile..."
+make -j8
